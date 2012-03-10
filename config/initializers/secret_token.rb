41e7eefedf45764837281a55107f6cb06958d116
Jorge Luna <jorge.luna@utexas.edu>
2012-03-09 22:24:31 -0600

    creating project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoRails::Application.config.secret_token = '4fd91e1249a0506a82f2f516595bc3111a2f0c55963fca1447ff8757b03fbdd855e6e05cd0df51bd8c2c53fc7b18071397fc9f09a93a9b0cac8c67051ae3b039'
